335a1377cf29fa6b9cf92d104b45be4f06d6e140
Roshni Patel <roshnipatel@Roshnis-MacBook-Pro.local>
2023-05-10 21:40:08 -0500

    done with models ??

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE'
+});
 
 // Categories have many Products
+Category.belongsTo(Product, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+})
 
 module.exports = {
   Product,
