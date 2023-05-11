91e609753975eb9eb72a5f5326a6d60db496db48
Roshni Patel <roshnipatel@Roshnis-MacBook-Pro.local>
2023-05-11 00:17:20 -0500

    category routes might be done ? idek atp

--- models/Product.js
+++ models/Product.js
@@ -25,7 +25,7 @@ Product.init(
       allowNull: false,
       validate: {
         isDecimal: true
-      }
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
@@ -33,15 +33,14 @@ Product.init(
       defaultValue: 10,
       validate: {
         isNumeric: true
-      }
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
-        key: 'id'
-      }
-    }
+      },
+    },
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -6,24 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'id',
+  foreignKey: 'category_id',
   onDelete: 'CASCADE'
 });
 
 // Categories have many Products
-Category.belongsTo(Product, {
-  foreignKey: 'id',
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
   onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag
+  through: ProductTag,
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag
+  through: ProductTag,
+  foreignKey: 'tag_id',
 })
 
 module.exports = {
